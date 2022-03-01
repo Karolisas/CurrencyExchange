21f96359223fdf6de1741a3d58845dc7db3b505f
karolis.gulbinas <karolisas@gmail.com>
2022-03-01 12:05:35 +0200

    init

---
+++ src/test/java/lt/karolis/microservices28/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.karolis.microservices28.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

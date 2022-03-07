c45224dc99afbecc68fb356c9f2cf3165ee1ab1f
karolis.gulbinas <karolisas@gmail.com>
2022-03-07 19:01:07 +0200

    Added logging for Zipkin log server || dependencies for zipkin

--- src/main/java/lt/karolis/microservices28/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/lt/karolis/microservices28/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,14 +1,19 @@
 package lt.karolis.microservices28.currencyexchangeservice.controller;
 
 import lt.karolis.microservices28.currencyexchangeservice.repository.ExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass().getSimpleName());
 
     @Autowired
     private Environment environment;
@@ -20,6 +25,8 @@ public class CurrencyExchangeController {
     public CurrencyExchange getExchangeRate(@PathVariable String from, @PathVariable String to) {
 
 //        CurrencyExchange curencyExchange = new CurrencyExchange(1000L, from, to, new BigDecimal(50));
+
+        logger.info("getExchangeRate called with {} to {} ", from ,to);
 
         CurrencyExchange curencyExchange = repository.findByFromAndTo(from, to);
         if (curencyExchange == null) {
